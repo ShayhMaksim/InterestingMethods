33081de9ca27e12dba0f883f3b8c3d418a0c1d6d
Maksim <44121541+sargaras@users.noreply.github.com>
2020-11-21 18:24:40 +0300

    Create methods.h

---
+++ methods.h
@@ -0,0 +1,11 @@
+std::list<int> lst={1,2,3,4,5,6};
+
+    auto first=std::stable_partition(lst.begin(),lst.end(),[](int v) { return v>3;});
+
+    lst.erase(first,lst.end());
+
+    //4,5,6 
+    for (auto i : lst)
+    {
+        std::cout << i <<std::endl;
+    }
