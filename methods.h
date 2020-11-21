b881a8db047f1338afb1433b7b95983385710cff
Maksim <44121541+sargaras@users.noreply.github.com>
2020-11-21 18:25:10 +0300

    Update methods.h

--- methods.h
+++ methods.h
@@ -1,3 +1,7 @@
+#include <algorithm>
+#include <list>
+#include <iostream>
+
 std::list<int> lst={1,2,3,4,5,6};
 
     auto first=std::stable_partition(lst.begin(),lst.end(),[](int v) { return v>3;});
